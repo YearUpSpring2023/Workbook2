53c194e78e097cd41d36f4a3d4e9ff0dbbc2a99b
mattchristenson <matt@rcc.team>
2023-04-25 07:52:12 -0500

    add lecturedemos folder with files

---
+++ LectureDemos/SecondCode.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var firstName;
+
+firstName = "Matt";
+
+var greeting;
+
+greeting = "Your name is " + firstName; // This line will assign the result of an expression to the greeting variable.
+
+console.log(greeting);
+
+firstName = "John";
+
+console.log("Your name is now " + firstName);
---
+++ LectureDemos/ThirdCode.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+var word, num;
+word = "banana";
+num = 12;
+num = num + 1; // num is now 13
+word = word + 1; // error! can't add 1
+ // to "banana"
+
+
+ console.log(num);
+ console.log(word);
